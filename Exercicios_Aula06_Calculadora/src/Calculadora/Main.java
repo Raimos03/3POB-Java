127e098553ef35780a25f4475cadf10719bdbdcd
pedro <pedrolimae3@gmail.com>
2022-10-03 02:11:53 -0300

    new

---
+++ Exercicios_Aula06_Calculadora/src/Calculadora/Main.java
@@ -0,0 +1,18 @@
+/*
+ * O programa deve mostrar o valor dividido para cada ocupante.
+ * Deve ser exibido o nome e valor de cada um.
+ * 
+ */
+
+package Calculadora;
+
+public class Main {
+	
+	 public static void main(String[] args) {
+		 	
+		Conta novaconta= new Conta(); // instancio um objeto
+		novaconta.GerenciaConta();		 	
+		return ;
+	 }
+	
+}
