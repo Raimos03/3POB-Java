c2d83160305967c8aaeb03723e1ac212e40a84c6
pedro <pedrolimae3@gmail.com>
2022-10-03 02:21:37 -0300

    oi

---
+++ Exercicio_Aula06_Calculadora/src/Calculadora/Pessoa.java
@@ -0,0 +1,22 @@
+package Calculadora;
+
+public class Pessoa {
+
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public Pessoa() { //construtor
+		
+	}
+	
+	public Pessoa(String nome) {
+		this.nome=nome;
+	}
+}
