e9f10419d188c943c7050c672c3a64913fbe574d
pedro <pedrolimae3@gmail.com>
2022-10-04 11:57:05 -0300

    av1

---
+++ Exercicio_Aula7_Av1/prova1/src/pacote/Produto.java
@@ -0,0 +1,107 @@
+package pacote;
+
+public class Produto {
+	
+	private int id;
+	private String CodBarras;
+	private String Sku;
+	private String nome;
+	private String descricao;
+	private String categoria;
+	private double preco;
+	private double peso;
+	private String fabricante;
+	 
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCodBarras() {
+		return CodBarras;
+	}
+
+	public void setCodBarras(String codBarras) {
+		CodBarras = codBarras;
+	}
+
+	public String getSku() {
+		return Sku;
+	}
+
+	public void setSku(String sku) {
+		Sku = sku;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+	public String getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+
+	
+	public Produto(){
+	}
+	
+	public Produto(int id, String codbarras,String sku, String nome,String descricao, String categoria, Double preco, Double peso, String fabricante){
+	
+		this.id=id;
+		this.CodBarras=codbarras;
+		this.Sku=sku;
+		this.nome=nome;
+		this.descricao=descricao;
+		this.categoria=categoria;
+		this.preco=preco;
+		this.peso=peso;
+		this.fabricante=fabricante;
+		
+	}
+}
---
+++ Exercicio_Aula7_Av1/prova1/src/pacote/main.java
@@ -0,0 +1,109 @@
+package pacote;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("--- Menu ----\n");
+		int id;
+		String CodBarras;
+		String Sku;
+		String nome;
+		String descricao;
+		String categoria;
+		double preco;
+		double peso;
+		String fabricante;
+		
+		
+		int i=1;
+        while(i!=0){
+            
+            Scanner sc = new Scanner(System.in);
+                    
+            System.out.println("\t-1 para inserir produto:\n");
+            System.out.println("\t-2 para alterar produto:\n");
+            System.out.println("\t-3 para excluir produto:\n");
+            System.out.println("\t-4 para listar um produto:\n");
+            System.out.println("\t-5 para listar todos os produtos:\n");
+            System.out.println("\t-0 para terminar:");
+            
+            i = sc.nextInt();
+            
+            if(i==0){
+                break;
+            }
+            
+            else if(i==1){
+                
+                System.out.println("-----inserir produto: ----- \n");
+                System.out.println("\tDigite o nome:");
+                String nome= sc.nextLine();
+                System.out.println("\tDigite a matricula:");
+                String matricula=sc.nextLine();
+                System.out.println("\tDigite o periodo:");
+                int periodo=sc.nextInt();
+                
+//                Aluno naluno= new Aluno(matricula,nome,periodo);
+//                
+//                lAluno.add(naluno); // add na lista de alunos
+                
+            }
+            
+            else if(i==2){
+                System.out.println("alterar produto\n");
+                
+            }
+            else if(i==3){
+                
+            		System.out.println("excluir produto\n");
+            	
+            	
+//            		Aluno aluno1= new Aluno("14521","Maria",3);
+//            		Aluno aluno2= new Aluno("11452","Rubens",5);
+//                 
+//            		Aluno aluno3= new Aluno("125452","Marcelo",8);
+//            		Aluno aluno4= new Aluno("125010","Felipe",2);
+//            		
+//            		//ArrayList<Aluno> alunos = new ArrayList<Aluno>();
+//            		
+//            		ArrayList<Aluno> alunos = new ArrayList<>();
+//            		
+//            		alunos.add(aluno1);
+//            		alunos.add(aluno2);
+//                 
+//            		Turma turma1 = new Turma("3POB","PROGRAMACAO","Andre","manha",disciplina,alunos);
+//            		
+//            		ArrayList<Aluno> alunos2 = new ArrayList<>();
+//            		
+//            		alunos2.add(aluno3);
+//            		alunos2.add(aluno4);
+//            		
+//            		Turma turma2 = new Turma("3ESD","eSTRUTURA","fERLIM","noite",disciplina2,alunos2);
+//            		
+//            		turma1.imprimeTurma();
+//            		turma2.imprimeTurma();
+            }
+            else if(i==4){ // i=4
+                
+                 System.out.println("listar um produto");
+            }
+            else if(i==5){ // i=4
+                
+                 System.out.println("listar todos os produtos");
+            }
+                       
+            sc.close();
+        }
+
+	}
+	
+	
+
+
+}
